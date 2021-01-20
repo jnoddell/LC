5333ae66cd8d68209f0965f62277fa719d0d1ffc
Justin Noddell <jnoddell20@gmail.com>
2021-01-20 09:22:21 -0500

    Create 965.py

--- E/965.py
+++ E/965.py
@@ -1,1 +1,25 @@
-
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isUnivalTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        vals = []
+        
+        def dfs( node ):
+            
+            if node:
+                vals.append(node.val)
+                dfs(node.left)
+                dfs(node.right)
+        
+        dfs(root)
+        
+        return len(set(vals)) == 1
